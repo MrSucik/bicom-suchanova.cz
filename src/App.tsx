83883d5964826dee2217cb6f2156da1772d5b93e
Daniel Suchan <mr.sucik@gmail.com>
2022-02-09 20:20:38 +0800

    Remove label from template

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,15 @@ import './App.css';
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
   const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
-  // Return the App component.
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <p>
           Page has been open for <code>{count}</code> seconds.
         </p>
