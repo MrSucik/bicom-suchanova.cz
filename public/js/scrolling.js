2a2e43fe0110f8dc2d87c5b0e2b07fabfb2aa3bd
Daniel Suchan <mr.sucik@gmail.com>
2023-04-26 23:13:12 +0200

    feat: remove unused content

--- public/js/scrolling.js
+++ public/js/scrolling.js
@@ -1,31 +1,20 @@
-function scrolla(a) {
-    document.getElementsByClassName("navigation__checkbox")[0].checked = false;
-    document.getElementsByClassName("modal__toggle")[0].checked = false;
-    if (navigator.userAgent.search("Firefox") >= 0) {
-        ScrollToResolver(document.getElementById(a));
-    }
-    else {
-        $("html, body").animate({
-            scrollTop: $(a).offset().top - 80
-        }, 1400)
-    }
-}
+const scrollTo = (selector) => {
+  document.getElementsByClassName("navigation__checkbox")[0].checked = false;
+  if (navigator.userAgent.search("Firefox") >= 0) {
+    ScrollToResolver(document.getElementById(selector));
+  } else {
+    $("html, body").animate({ scrollTop: $(selector).offset().top - 80 }, 1400);
+  }
+};
+
 function ScrollToResolver(elem) {
-    var jump = parseInt(elem.getBoundingClientRect().top * .2);
-    document.body.scrollTop += jump;
-    document.documentElement.scrollTop += jump;
-    if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
-        elem.lastjump = Math.abs(jump);
-        setTimeout(function () {
-            ScrollToResolver(elem);
-        }, "30");
-    }
-    else {
-        elem.lastjump = null;
-    }
-}
-function openModal(){
-    document.getElementsByClassName("modal__toggle")[0].checked = true;
-}function closeModal(){
-    document.getElementsByClassName("modal__toggle")[0].checked = false;
+  let jump = parseInt(elem.getBoundingClientRect().top * 0.2);
+  document.body.scrollTop += jump;
+  document.documentElement.scrollTop += jump;
+  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+    elem.lastjump = Math.abs(jump);
+    setTimeout(() => void ScrollToResolver(elem), 30);
+  } else {
+    elem.lastjump = null;
+  }
 }
